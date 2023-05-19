777515b39f5cf47893c83a2c74f9d2bd5b6a6df9
Jason <jason.190@163.com>
2023-05-19 16:21:24 +0800

    core

---
+++ WouldTimeConverter/WouldTimeConverter/core/converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WouldTimeConverter.core;
+
+public class converter
+{
+    private DateTimeOffset inputDateTime;
+    private DateTimeOffset outputDateTime;
+    private TimeZoneInfo outputDateTimeZoneInfo;
+
+    public DateTimeOffset InputDateTime { get => inputDateTime; set => inputDateTime = value; }
+    public DateTimeOffset OutputDateTime { get => outputDateTime; set => outputDateTime = value; }
+    public TimeZoneInfo OutputDateTimeZoneInfo { get => outputDateTimeZoneInfo; set => outputDateTimeZoneInfo = value; }
+
+    public converter(DateTimeOffset input) { 
+        InputDateTime = input;
+    }
+    public converter() { 
+        InputDateTime= DateTimeOffset.Now;
+    }
+    public void setOutTimeZoneInfo(TimeZoneInfo target)
+    {
+        OutputDateTimeZoneInfo = target;
+    }
+    public DateTimeOffset setInputDateTime(DateTimeOffset time) {
+        return InputDateTime = time;
+    }
+
+    public DateTimeOffset ConverterTime(DateTimeOffset inputTime,TimeZoneInfo outputTimeZoneInfo)
+    {
+        setInputDateTime(inputTime);
+        setOutTimeZoneInfo(outputTimeZoneInfo);
+
+        DateTimeOffset targetTime = TimeZoneInfo.ConvertTime(InputDateTime, OutputDateTimeZoneInfo);
+
+        return targetTime;
+    }
+}
---
+++ WouldTimeConverter/WouldTimeConverter/viewmodel/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System;
+using System.Linq;
+
+namespace WouldTimeConverter.viewmodel;
+
+public class MainViewModel: ObservableObject
+{
+    string? inTimeZone,outTimeZone;
+    string? inHour, inMin, inSec, outHour, outMin, outSec;
+
+    string[]? years, months, days, hours, minutes,inTimeZoneArray,outTimeZoneArray;
+
+    public string InTimeZone { get => inTimeZone; set => inTimeZone = value; }
+    public string OutTimeZone { get => outTimeZone; set => outTimeZone = value; }
+    public string InHour { get => inHour; set => inHour = value; }
+    public string InMin { get => inMin; set => inMin = value; }
+    public string InSec { get => inSec; set => inSec = value; }
+    public string OutHour { get => outHour; set => outHour = value; }
+    public string OutMin { get => outMin; set => outMin = value; }
+    public string OutSec { get => outSec; set => outSec = value; }
+    public string[] Years { get => years; set => years = value; }
+    public string[] Months { get => months; set => months = value; }
+    public string[] Days { get => days; set => days = value; }
+    public string[] Hours { get => hours; set => hours = value; }
+    public string[] Minutes { get => minutes; set => minutes = value; }
+    public string[] InTimeZoneArray { get => inTimeZoneArray; set => inTimeZoneArray = value; }
+    public string[] OutTimeZoneArray { get => outTimeZoneArray; set => outTimeZoneArray = value; }
+
+    public MainViewModel() {
+        init();
+    }
+    void init()
+    {
+        for (int i = 1; i <= 31;i++)
+        {
+            Years[i - 1] = (1995 + i).ToString();
+            if (i <= 12)
+            {
+                Months[i-1] = i.ToString();
+            }
+            Days[i] = i.ToString(); 
+        }
+        for(int i = 0; i <= 59; i++)
+        {
+            if (i <= 23)
+            {
+                Hours[i] = (i + 1).ToString();
+
+            }
+            Minutes[i] = i.ToString();
+        }
+
+        foreach (TimeZoneInfo item in TimeZoneInfo.GetSystemTimeZones())
+        {
+            InTimeZoneArray.Append(item.DisplayName);
+            OutTimeZoneArray.Append(item.DisplayName);
+        }
+    }
+}
